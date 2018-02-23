33f9c69c5b1dc484e4cccbaa165a3dd4f16e49d3
johnpolacek <johnpolacek@gmail.com>
2018-02-23 15:38:08 -0600

    import cleanup

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
 import { Box, H2, Text, A } from 'styled-system-html';
 import SiteFooter from '../components/SiteFooter'
 
-
-class About extends Component {
-	constructor() {
-		super();
-	}
-
-	render() {
-		return (
-			<Box>
-				<Box p={6} mw="1200px" mx="auto" align="center">
-					<H2 pt={5} pb={4}>About Page</H2>
-					<Text pb={5}>For more info, go to the <A href="https://johnpolacek.github.io/styled-starter/">Styled Starter Project</A></Text>
-				</Box>
-				<SiteFooter />
-			</Box>
-		);
-	}
-}
-
-export default About;
+export default (props) => (
+	<Box>
+		<Box p={6} mw="1200px" mx="auto" align="center">
+			<H2 pt={5} pb={4}>About Page</H2>
+			<Text pb={5}>For more info, go to the <A href="https://johnpolacek.github.io/styled-starter/">Styled Starter Project</A></Text>
+		</Box>
+		<SiteFooter />
+	</Box>
+)
