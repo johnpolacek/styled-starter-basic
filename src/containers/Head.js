da174658e95a167ffc391264773ccfb093083b72
johnpolacek <johnpolacek@gmail.com>
2018-03-06 06:22:06 -0600

    use system font as default

--- src/containers/Head.js
+++ src/containers/Head.js
@@ -8,6 +8,5 @@ export default (props) => (
 	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	    <meta name="theme-color" content="#000000" />
 	    <title>Styled Starter | Starter Kit with React, Next.js and Styled Components</title>
-	    <link href="https://fonts.googleapis.com/css?family=Nunito:400,700,900" rel="stylesheet" />
     </Head>
 )
